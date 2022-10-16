dd82ccaa40050908dff8194f1951c981219dc828
Toml1100 <77038859+Toml1100@users.noreply.github.com>
2022-10-16 11:06:50 -0700

    Create reserve.php

---
+++ reserve.php
@@ -0,0 +1,5 @@
+<!-- 
+Prints out all of the previously selected Information and the total estimated price
+Collects Name, Email and Phone Number
+Submits to vardump.php (or similar page) that displays name, email, phone, set, package, date, extras (these should all be available in the GET or POST array) 
+-->
