434abdf37f90abb0862edf5b4afb3014d344331e
Toml1100 <77038859+Toml1100@users.noreply.github.com>
2022-10-16 11:04:13 -0700

    Create packages.php

---
+++ packages.php
@@ -0,0 +1,5 @@
+<!-- 
+Checks if the date/package is available using PHP** - if not available, it presents a nice message saying sorry, links back to "pick your set"
+If its available, allows user to visually pick between the available packages
+Submits to extras.php 
+-->
