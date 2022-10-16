6e48fc1bf70969e0e1c245846355a01a0529b7a9
Toml1100 <77038859+Toml1100@users.noreply.github.com>
2022-10-16 11:05:24 -0700

    Create extras.php

---
+++ extras.php
@@ -0,0 +1,6 @@
+<!-- 
+Only shows the available extras using PHP**
+If its available, allows user to visually pick between the available packages
+Delivery is also an option at the bottom, looks visually different
+Submits to reserve.php 
+-->
